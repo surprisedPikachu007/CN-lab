4351c5de25e652bc41206d5443d6ec28d64da96e
surprisedPikachu69 <arvind7447@gmail.com>
2022-12-04 11:47:25 +0530

    basic network commands

---
+++ 1/commands.bat
@@ -0,0 +1,11 @@
+tcpdump
+
+netstat
+
+ipconfig
+
+nslookup github.com
+
+tracert github.com
+
+ping 192.168.0.1 -c 4
---
+++ 1/commands.sh
@@ -0,0 +1,11 @@
+tcpdump
+
+netstat
+
+ifconfig
+
+nslookup github.com
+
+traceroute github.com
+
+ping ping 192.168.0.1 -c 4
